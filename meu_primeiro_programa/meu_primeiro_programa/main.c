b5e585e8940c28fe12133d28964346a8cf6273c8
Rodrigo Matos <rodrigo98matos@gmail.com>
2021-08-21 01:37:57 -0300

    add

---
+++ meu_primeiro_programa/meu_primeiro_programa/main.c
@@ -0,0 +1,18 @@
+/*
+ * meu_primeiro_programa.c
+ *
+ * Created: 19/08/2021 19:16:06
+ * Author : Alberto.ncc
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
