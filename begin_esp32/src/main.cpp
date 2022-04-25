580a1c807734ca335ca0944248ba3860e0c4fb10
Rodrigo Matos <rodrigo98matos@gmail.com>
2022-04-24 21:23:39 -0300

    Add

--- begin_esp32/src/main.cpp
+++ begin_esp32/src/main.cpp
@@ -2,14 +2,14 @@
 
 void setup() {
   Serial.begin(115200);
-  pinMode(2,OUTPUT);
+  pinMode(21,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Helo, World!");
-  digitalWrite(2,1);
+  digitalWrite(21,1);
   delay(1000);
-  digitalWrite(2,0);
+  digitalWrite(21,0);
   delay(1000);
 }
